807d2b97025d84536188fe9a34b2cd29c921fa0d
Rémi Durieu <mail@remidurieu.dev>
2025-01-21 17:58:40 +0100

    Step 1 - Basic authors CRUD

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,44 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.all
   end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to [ :edit, @author ], notice: "Author created!"
+    else
+      flash.now[:alert] = "Failed to create author!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to [ :edit, @author ], notice: "Author updated!"
+    else
+      flash.now[:alert] = "Failed to update author!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+    redirect_to [ :authors ]
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
 end
